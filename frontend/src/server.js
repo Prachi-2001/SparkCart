c64d15f4f2cc07f31af3d43a56d97c5e274bd3bd
Prachi-2001 <prachipachang164@gmail.com>
2023-09-08 21:25:27 +0530

    updated frontend

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,8 +1,8 @@
 // export const server = "http://localhost:5000/api/v2";
-export const server = "https://spark-backend-gbknwsx61-prachi-2001.vercel.app/api/v2";
+export const server = "https://spark-backend.vercel.app/api/v2";
 
 
-export const backend_url = "https://spark-backend-gbknwsx61-prachi-2001.vercel.app";
+export const backend_url = "https://spark-backend.vercel.app/";
 // export const backend_url = "http://localhost:5000"
 
 // export const //server = "http://localhost:5000/api/v2";
