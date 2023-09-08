f0340a6a0cf6b4f33d883e987087460495268fea
Prachi-2001 <prachipachang164@gmail.com>
2023-09-08 23:10:47 +0530

    updated frontend

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -8,7 +8,6 @@ const sendToken = (user,statusCode,res) =>{
     const options = {
         expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
         httpOnly: true,
-        sameSite: "none",
     }
 
     res.status(statusCode).cookie("token", token, options).json({
