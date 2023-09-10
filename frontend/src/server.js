ff395b4ee5266439f91efab3442e7d2b0c715910
Prachi-2001 <prachipachang164@gmail.com>
2023-09-10 13:33:44 +0530

    updated server url done

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,10 +1,6 @@
-export const server = "http://localhost:5000/api/v2";
-// export const server = "https://spark-backend.vercel.app/api/v2";
+// export const server = "http://localhost:5000/api/v2";
+export const server = "https://spark-backend.vercel.app/api/v2";
 
 
-// export const backend_url = "https://spark-backend.vercel.app";
-export const backend_url = "http://localhost:5000"
-
-// export const //server = "http://localhost:5000/api/v2";
-
-// export const backend_url = "http://localhost:5000";
+export const backend_url = "https://spark-backend.vercel.app";
+// export const backend_url = "http://localhost:5000"
