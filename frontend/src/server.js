7a9a4f419fbaacb2548c75e27fa3435a8849202f
Prachi-2001 <prachipachang164@gmail.com>
2023-09-10 17:01:39 +0530

    updated backend

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,5 +1,5 @@
 // export const server = "http://localhost:5000/api/v2";
-export const server = "https://sparkcart.onrender.com";
+export const server = "https://sparkcart.onrender.com/api/v2";
 
 
 export const backend_url = "https://sparkcart.onrender.com";
